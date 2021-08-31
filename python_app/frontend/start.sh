ece732ca4a9f7ed3500e195aaf9ba17b9320312f
ymjoung <ymjoung@amazon.com>
2021-08-31 14:55:58 +0900

    update frontend start.sh

--- python_app/frontend/start.sh
+++ python_app/frontend/start.sh
@@ -11,7 +11,7 @@ echo $REGION && echo ''
 #export region="us-east-1"
 
 #cat configs.json.tpl | sed -i "s/REGION_CODE/'$region'/g" > configs.json
-cat configs.json.tpl | sed "s/REGION_CODE/'$region'/g" > configs.json
+cat configs.json.tpl | sed "s/REGION_CODE/'$REGION'/g" > configs.json
 
 source v_demo_app/bin/activate
 pip3 install -r requirements.txt

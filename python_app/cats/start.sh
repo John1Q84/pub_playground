94d361e2b84885f4e95b64103f60816dc81d3b04
ymjoung <ymjoung@amazon.com>
2021-09-01 02:31:23 +0900

    update start.sh for cats and dogs

--- python_app/cats/start.sh
+++ python_app/cats/start.sh
@@ -8,11 +8,13 @@ echo '>> Get Region ....'
 REGION=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN"  http://169.254.169.254/latest/dynamic/instance-identity/document/ | grep region | cut -d \" -f 4`
 echo $REGION && echo ''
 
+HERE=$(dirname $(realpath -s $0))
+echo $HERE
 
 #cat configs.json.tpl | sed -i "s/REGION_CODE/'$region'/g" > configs.json
 cat configs.json.tpl | sed "s/REGION_CODE/'$region'/g" > configs.json
 
-source v_demo_app/bin/activate
+source "$HERE/v_demo_app/bin/activate"
 pip3 install -r requirements.txt
 
 python3 app.py

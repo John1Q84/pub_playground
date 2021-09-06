86653eaf29f93afdf56e8ec8d43ac6e59e2fdaca
ymjoung <ymjoung@amazon.com>
2021-09-07 01:33:29 +0900

    update frontend app.py

--- python_app/frontend/app.py
+++ python_app/frontend/app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # testing code
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, Response
 from flask_restx import Api, Resource
 
-import json, datetime
+import json, datetime, requests
 
 with open('configs.json', 'r') as f:    # config file loading
     configs = json.load(f)
 
 app = Flask (__name__)
 api = Api(app)
-
+SITE_NAME = 'http://service.mydomain.int:80'
 
 # @app.route('/demo')
 # def hello_world():
@@ -26,11 +26,18 @@ def index():
         region = configs['INFO']['REGION']
     )
 
-@app.route('/service')
-def google():
+# @app.route('/service')
+# def service():
     return redirect("http://service.mydomain.int/service") # backend serviced의 endpoint, 실습에서는 hardcording으로 수행
 
-
+@app.route('/<path:path>',methods=['GET'])
+def proxy(path):
+    if request.method=='GET':
+        resp = requests.get(f'{SITE_NAME}{path}')
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code, headers)
+    return response
 
 # @api.route('/hello')
 # class InfoReturn(Resource):

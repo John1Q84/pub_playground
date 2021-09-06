d68b8120507916c857ef5b6bfe1edd43ffb594bc
ymjoung <ymjoung@amazon.com>
2021-09-07 02:15:11 +0900

    update frontend app.py

--- python_app/frontend/app.py
+++ python_app/frontend/app.py
@@ -34,9 +34,9 @@ def index():
 def proxy(path):
     if request.method=='GET':
         resp = requests.get(f'{SITE_NAME}{path}')
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
+        # excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        # headers = [(name, value) for (name, value) in     resp.raw.headers.items() if name.lower() not in excluded_headers]
+        response = Response(resp.content, resp.status_code)
     return response
 
 # @api.route('/hello')

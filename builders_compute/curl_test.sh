e8e7f3eec280a5fe02d8b9d9fff58c893d25b579
Joung <ymjoung@3c06304e2971.ant.amazon.com>
2022-03-29 23:12:40 +0900

    update curl test script

--- builders_compute/curl_test.sh
+++ builders_compute/curl_test.sh
@@ -7,11 +7,8 @@ then
     exit 1;
 fi
 while true; do
-    for ((i=0; i<6; i++))
-    do
-     ## put your alb dns name to the below line
-        echo `curl -H 'Cache-Control: no-cache' http://$ALB_DNS/service` 1 > /dev/null ;
-        sleep 0.2;
-    done
+    ## put your alb dns name to the below line
+    echo `curl -H 'Cache-Control: no-cache' http://$ALB_DNS/service` 1 > /dev/null ;
+    sleep 0.2;
 done
 

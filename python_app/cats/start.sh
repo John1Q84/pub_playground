a94622d297e169fcaf352c60631de40a7a1a5b50
Youngmin Joung <ymjoung@amazon.com>
2023-09-04 22:59:26 +0900

    update start.sh

--- python_app/cats/start.sh
+++ python_app/cats/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo 'get temporary token for metedata'
-TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+export TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 echo ''
 
 echo '>> Get Region ....'
-REGION=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN"  http://169.254.169.254/latest/dynamic/instance-identity/document/ | grep region | cut -d \" -f 4`
+export REGION=`curl -s -H "X-aws-ec2-metadata-token: $TOKEN"  http://169.254.169.254/latest/dynamic/instance-identity/document/ | grep region | cut -d \" -f 4`
 echo $REGION && echo ''
 
 HERE=$(dirname $(realpath -s $0))
